1ee5b1fceb20ccca57ef15edd00ac0afa600f5e2
long phan <longphan@nbp-144-141.nbp.ruw.rutgers.edu>
2016-02-14 17:57:10 -0500

    added orm methods

--- config/orm.js
+++ config/orm.js
@@ -2,9 +2,27 @@ var connection= require('./connection.js');
 
 
 var orm {
-
-
-
+  addBurger: function(burgerName, cb) {
+    var query = "INSERT INTO burgers (burger_name, devoured) VALUES (?, FALSE)";
+    connection.query(query, [burgerName], function(err, res) {
+      if(err) throw err;
+      cb(res);
+    });
+  }
+  devourBurger: function((burgerName, cb) {
+    var query = "UPDATE burgers SET devoured=true WHERE burger_name=?";
+    connection.query(query, [burgerName], function(err, res) {
+      if (err) throw  err;
+      cb(res);
+    });
+  }
+  showBurgers: function(tableInput, cb) {
+    var query = "SELECT * FROM ?";
+    connection.query(query, [tableInput], function(err, res) {
+      if(err) throw err;
+      cb(res);
+    });
+  } 
 
 }
 
