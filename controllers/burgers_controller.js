0522bace4a8f93749305a54c967a8ecccdb4debc
long phan <longphan@nbp-144-141.nbp.ruw.rutgers.edu>
2016-02-14 21:02:49 -0500

    added routes in burger controller js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,26 @@
 var express = require("express");
 var router = express.Router();
-var burger = require('./models/burger.js');
+var burger = require('../models/burger.js');
+
+
+router.get('/', function (req, res) {
+  burger.showBurgers(function (burgers_data) {
+    res.render('index', {burgers_data});
+  });
+});
+
+router.get('/add', function (req, res) {
+  burger.addBurger(req.body.burger_name, function (result) {
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+router.get('/devour', function (req, res) {
+  burger.devourBurger(req.body.burger_name, function (result) {
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
