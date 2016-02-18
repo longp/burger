4635d194389665b9e91f259c6ff6be1bd4ae33d9
long phan <longphan@longs-MacBook-Pro.local>
2016-02-18 02:20:55 -0500

    removed console.log from controller js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,20 +6,19 @@ var burger = require('../models/burger.js');
 router.get('/', function (req, res) {
   burger.showBurgers(function (burgers_data) {
     res.render('index', {burgers_data});
-    console.log(burgers_data[0].id)
   });
 });
 
 router.post('/add', function (req, res) {
   burger.addBurger(req.body.burger_name, function (result) {
-    console.log(result);
+   
     res.redirect('/');
   });
 });
 
 router.post('/devour', function (req, res) {
   burger.devourBurger(req.body.id, function (result) {
-    console.log(result);
+    
     res.redirect('/');
   });
 });
